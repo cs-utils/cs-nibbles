5f458f3019c086c6660e72e7ae818a2e7306d35b
Raymond de Vos <devosray@gmail.com>
2017-09-19 20:43:37 +0200

    Basic server <-> GUI integration

--- cs-nibbles.go
+++ cs-nibbles.go
@@ -12,7 +12,7 @@ func main() {
 	ADDRESS := ":8080"
 	BOARD_WIDTH := 100
 	BOARD_HEIGHT := 100
-	TICKRATE := (1 * time.Second) / 60
+	TICKRATE := (1 * time.Second) / 20
 
 	log.Info("Starting server on " + ADDRESS)
 	server := server.CreateServer(ADDRESS, BOARD_WIDTH, BOARD_HEIGHT, TICKRATE)
