03949e3262e515a530a5532969625147aefb4c3e
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-09-19 16:52:56 +0000

    Board demo

---
+++ static/main.js
@@ -0,0 +1,21 @@
+let b = new Board("gameScreen", 500, 500, 5);
+
+b.drawFromSnakeStrings("15,0,0,0,1;17,50,50,50,51,50,52");
+
+let s = "15";
+
+
+demo();
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+    for (let i = 0; i < 500; i++) {
+
+        s += ",30," + i;
+        b.drawFromSnakeStrings(s);
+        await sleep(50);
+    }
+}
